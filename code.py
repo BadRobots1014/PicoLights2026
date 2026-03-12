ec418b0119b91975a7a6a6683ecaa42e09501a3c
roDEV0 <gian@romanodev.com>
2026-03-11 20:24:46 -0400

    Initial Commit

---
+++ code.py
@@ -0,0 +1,35 @@
+import board
+from rainbowio import colorwheel
+import neopixel
+
+pixel_pin = board.GP5
+num_pixels = 3
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False)
+pulse = pulseio.PulseIn(board.DP3)
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+
+while True:
+    if len(pulse) == 0:
+        continue
+    display = round(pulse[0]/20)
+    match display:
+        case 0:
+            pixels.fill(RED)
+        case 1:
+            pixels.fill(YELLOW)
+        case 2:
+            pixels.fill(GREEN)
+        case 3:
+            pixels.fill(CYAN)
+        case 4:
+            pixels.fill(BLUE)
+        case 5:
+            pixels.full(PURPLE)
+    pixels.show()
