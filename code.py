41d18ac0f073e359308c9148183772e092f3f7ad
roDEV0 <gian@romanodev.com>
2026-03-14 19:15:35 -0400

    Pattern Functions

--- code.py
+++ code.py
@@ -10,6 +10,30 @@ num_pixels = 108
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.4, auto_write=False)
 pulse = pulseio.PulseIn(board.GP22, 5)
 
+def color_chase(color, wait):
+    for i in range(num_pixels):
+        pixels[i] = color
+        time.sleep(wait)
+        pixels.show()
+    time.sleep(0.5)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            rc_index = (i * 256 // num_pixels) + j
+            pixels[i] = colorwheel(rc_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+def color_flash(color, wait):
+    pixels.fill(color)
+    pixels.show()
+    time.sleep(wait)
+    pixels.fill((0, 0, 0))
+    pixels.show()
+    time.sleep(wait)
+
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
 GREEN = (0, 255, 0)
